f95e468bc6c6f308da452d8277346857865e2c9b
Seppo Enarvi <seppo.enarvi@aalto.fi>
2011-02-15 08:46:01 +0000

    Use the binaries that are in the current working directory.

--- rec.sh
+++ rec.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-export LD_LIBRARY_PATH="/share/puhe/x86_64/lib"
+#export LD_LIBRARY_PATH=/share/puhe/x86_64/lib
 
-cd /home/thirsima/Work/online-demo-libs
+#cd /home/thirsima/Work/online-demo-libs
 
 dir=/home/thirsima/Work/online-demo-libs
 lm=$dir/morph40000_6gram_gt_1e-8.bin
@@ -12,5 +12,3 @@ lex=$dir/morph40000.lex
 hmm=$dir/mfcc_cms_adapt_speecon_tri_14.6.2006_9
 
 ./recognizer -b $hmm -d "./decoder --binlm $lm --lookahead $la_lm --lexicon $lex --ph $hmm.ph --dur $hmm.dur --lm-scale 35 --token-limit 30000 --beam 200"
-
-
